7a00b54cd275b328027f86501b376eb30beb6657
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-11-29 17:06:08 -0300

    Update Biblioteca.sql

--- Biblioteca.sql
+++ Biblioteca.sql
@@ -11,7 +11,10 @@ CREATE TABLE IF NOT EXISTS Livros(
 );
 
 INSERT INTO Livros (titulo, autor, ano_publicacao, genero, quantidade_disponivel)
-VALUES ('O Código Da Vinci', 'Dan Brown', 2003, 'Romance', 20);
+VALUES ('O código Da Vinci', 'Dan Brown', 2003, 'romance', 20);
+
+INSERT INTO Livros (titulo, autor, ano_publicacao, genero, quantidade_disponivel)
+VALUES ('Harry Potter e a Pedra Filosofal', 'J K Rowling', 1997, 'Fantasia', 50);
 
 CREATE TABLE IF NOT EXISTS Usuarios(
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,14 +27,14 @@ CREATE TABLE IF NOT EXISTS Usuarios(
 INSERT INTO Usuarios (nome, cpf, email, telefone)
 VALUES ('Manoel', '12345678910', 'manoel@gmail.com', '912345678');
 
+INSERT INTO Usuarios (nome, cpf, email, telefone)
+VALUES ('Claudia', '12345678980', 'claudia@gmail.com', '942345678');
+
 SHOW TABLES;
 
 DESCRIBE Livros;
-
 DESCRIBE Usuarios;
 
-DROP TABLE IF EXISTS Usuarios;
+SELECT * FROM Livros;
 
-SELECT * FROM Usuarios;
-
-UPDATE Livros SET genero = 'Suspense' WHERE id_livro = 1;
+UPDATE Livros SET genero = 'Romance' WHERE id_livro = 1;
